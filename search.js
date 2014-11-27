f8472378e06cadbbcb597586486f08a949fc356a
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2014-11-27 20:05:55 +0200

    add clickable list items, better UI

--- search.js
+++ search.js
@@ -1,6 +1,9 @@
 $(function(){
   var $input = $('#input'),
-      $results = $('#results');
+      $results = $('#results'),
+      $currentMovie = $('#current-movie'),
+      $resultItems,
+      clicks;
 
   /*== STREAM CREATION ==*/
 
@@ -13,7 +16,6 @@ $(function(){
       return text.length > 2; // only search for words that have more than 2 letters
     });
 
-
   var debounced = keyups
     .debounce(500); /* Add a small latency of 500ms */
 
@@ -23,10 +25,50 @@ $(function(){
 
   /*== HELPERS ==*/
 
+  /* Ajax functions */
   function searchOMDB(term) {
     return $.ajax({
       url: 'http://www.omdbapi.com/?s='+term
     }).promise();
+  }
+
+  function getMovieData(clicked) {
+    return $.ajax({
+      url: 'http://www.omdbapi.com/?i=' + clicked.currentTarget.id
+    }).promise();
+  }
+
+  /* DOM modification functions */
+  function createListElement(movie) {
+    return $('<li>').text(movie.Title+' (' + movie.Type + ') ')
+      .attr('class', 'result-item')
+      .attr('id', movie.imdbID)
+      .append($('<a>')
+        .text('link')
+        .attr('href', 'http://www.imdb.com/title/' + movie.imdbID));
+  }
+
+  function showMovieOnDOM(movie) {
+
+    function createULElement(title, list) {
+      return ($('<h3>').text(title))
+        .append($('<ul>')
+          .attr('class', 'no-bullet')
+          .append($.map(list,
+            function (element) {
+              return $('<li>').text(element);
+            })));
+    }
+
+    $currentMovie
+      .empty()
+      .append($('<h2>').text(movie.Title + ' (' + movie.Year + ')'))
+      .append($('<h3>').text('Plot'))
+      .append($('<p>').text(movie.Plot))
+      .append(createULElement('Writers', movie.Writer.split(', ')))
+      .append(createULElement('Directors', movie.Director.split(', ')))
+      .append(createULElement('Actors', movie.Actors.split(', ')))
+      .append(createULElement('Genre', movie.Genre.split(', ')))
   }
 
   var movies = distinct
@@ -43,17 +85,29 @@ $(function(){
         .append($('<li>').text("No results found."))
       } else {
         $('#results')
-        .empty()
-        .append ($.map(parsed.Search, function (value) {
-          return $('<li>').text(value.Title+' (' + value.Type + ') ')
-            .append($('<a>')
-              .text("link")
-              .attr("href", "http://www.imdb.com/title/"+value.imdbID));
-        }));
+          .empty()
+          .append ($.map(parsed.Search, function (movie) {
+            return createListElement(movie);
+          }));
+
+        $resultItems = $('.result-item');
+        clicks = Rx.Observable.fromEvent($resultItems, 'click');
+
+        clicks
+          .flatMapLatest(getMovieData)
+          .subscribe(
+          function (data) {
+            var movie = JSON.parse(data);
+            console.log(movie);
+            showMovieOnDOM(movie);
+          },
+          function (error) {
+            console.log(error);
+          });
       }
     },
     function (error) {
       $results.empty().append($('<li>').text('Error calling OMDB:' + error));
+      clicks.dispose();
     });
-
-});
+}).foundation();
